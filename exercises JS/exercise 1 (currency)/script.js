cf2b54bf30fdaf46bde6c137653cd40c0c83e39a
PriscilaCodrington <priscilacodrington@gmail.com>
2023-02-08 20:42:00 +0100

    Exercise 1 and 2 complete

---
+++ exercises JS/exercise 1 (currency)/script.js
@@ -0,0 +1,15 @@
+/*
+  CURRENCY FORMATTING
+  ===================
+  The business is breaking out into a new market and need to convert prices to USD
+  Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
+*/
+
+function convertToUSD() {
+        let pounds = prompt("Amount in Pounds");
+        let USD = pounds * 1.4;
+        return (USD);
+
+}
+
+console.log(convertToUSD());
